e4014a44b670a1d5d3e0d0222b3d848108c7737f
Timea Magyar <tmagyar@thinkport.digital>
2023-10-02 22:39:48 +0200

    provide account and region information in vpc lookup

--- infrastructure/app.py
+++ infrastructure/app.py
@@ -1,10 +1,11 @@
 # https://konem.blog/2021/01/17/how-to-structure-your-application-architecture-using-aws-cdk/
-from aws_cdk import App
+from aws_cdk import App, Environment, CfnParameter, Aws
 
 from domain.sagemaker_domain_stack import SagemakerDomainStack
 from vpc.vpc_stack import VPCStack
 
 app = App()
+env_EU = Environment(account=f"{Aws.ACCOUNT_ID}", region=f"{Aws.REGION}")
 vpc_stack = VPCStack(app, "VPCStack")
-sagemaker_domain_stack = SagemakerDomainStack(app, "SagemakerDomainStack")
+sagemaker_domain_stack = SagemakerDomainStack(app, "SagemakerDomainStack", env_EU)
 app.synth()
