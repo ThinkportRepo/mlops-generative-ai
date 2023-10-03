952b1a11b6ed9f946c415d4deb45d70eb2d6ba2a
Timea Magyar <tmagyar@thinkport.digital>
2023-10-03 12:17:14 +0200

    add cdk.context.json

--- infrastructure/app.py
+++ infrastructure/app.py
@@ -8,7 +8,7 @@ from domain.sagemaker_domain_stack import SagemakerDomainStack
 from vpc.vpc_stack import VPCStack
 
 app = App()
-vpc_stack = VPCStack(app, "VPCStack")
 env_default = Environment(account=os.environ["CDK_DEFAULT_ACCOUNT"], region=os.environ["CDK_DEFAULT_REGION"])
+vpc_stack = VPCStack(app, "VPCStack", env=env_default)
 sagemaker_domain_stack = SagemakerDomainStack(app, "SagemakerDomainStack", env=env_default)
 app.synth()
