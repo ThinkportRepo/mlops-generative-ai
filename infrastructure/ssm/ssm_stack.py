36a6c2d2277c818a8ed50bbd70cd2c8913c33efe
Timea Magyar <tmagyar@thinkport.digital>
2023-10-03 17:31:22 +0200

    fix typo in ssm stack

--- infrastructure/ssm/ssm_stack.py
+++ infrastructure/ssm/ssm_stack.py
@@ -1,5 +1,5 @@
 from aws_cdk import Stack
-from aws_cdk.aws_ec2 import Vpc
+from aws_cdk.aws_ec2 import Vpc, SubnetType, SubnetSelection, Subnet
 from aws_cdk.aws_ssm import StringParameter
 from constructs import Construct
 
@@ -13,17 +13,21 @@ class SSMStack(Stack):
                                  string_value=vpc.vpc_id
                                  )
 
+
+        public_subnet0 = vpc.public_subnets[0]
+
+
         vpc_public_subnets = StringParameter(self, "MLOpsVpcPublicSubnets",
                                              parameter_name="/mlops/vpc/public/subnet/id",
-                                             string_value=str(vpc.public_subnets)
+                                             string_value=public_subnet0.subnet_id
                                              )
-
-        vpc_private_subnets = StringParameter(self, "MLOpsVpcPrivateSubnets",
-                                              parameter_name="/mlops/vpc/private/subnet/id",
-                                              string_value=str(vpc.private_subnets)
-                                              )
-
-        vpc_isolated_subnets = StringParameter(self, "MLOpsVpcIsolatedSubnets",
-                                               parameter_name="/mlops/vpc/isolated/subnet/id",
-                                               string_value=str(vpc.isolated_subnets)
-                                               )
+        #
+        # vpc_private_subnets = StringParameter(self, "MLOpsVpcPrivateSubnets",
+        #                                       parameter_name="/mlops/vpc/private/subnet/id",
+        #                                       string_value=vpc.private_subnets
+        #                                       )
+        #
+        # vpc_isolated_subnets = StringParameter(self, "MLOpsVpcIsolatedSubnets",
+        #                                        parameter_name="/mlops/vpc/isolated/subnet/id",
+        #                                        string_value=vpc.isolated_subnets
+        #                                        )
