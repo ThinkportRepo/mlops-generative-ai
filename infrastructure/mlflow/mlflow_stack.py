c554c8025d8d57ad5a96eddb40416f4b92a3a274
Timea Magyar <tmagyar@thinkport.digital>
2023-12-28 15:50:20 +0100

    readd mlflow stack

--- infrastructure/mlflow/mlflow_stack.py
+++ infrastructure/mlflow/mlflow_stack.py
@@ -36,21 +36,6 @@ class MLflowStack(Stack):
         service_name = "mlflow"
 
         # ==================================================
-        # ================= IAM ROLE =======================
-        # ==================================================
-        role = iam.Role(
-            scope=self,
-            id="TASKROLE",
-            assumed_by=iam.ServicePrincipal(service="ecs-tasks.amazonaws.com"),
-        )
-        role.add_managed_policy(
-            iam.ManagedPolicy.from_aws_managed_policy_name("AmazonS3FullAccess")
-        )
-        role.add_managed_policy(
-            iam.ManagedPolicy.from_aws_managed_policy_name("AmazonECS_FullAccess")
-        )
-
-        # ==================================================
         # ================== SECRET ========================
         # ==================================================
         db_password_secret = sm.Secret(
@@ -102,74 +87,8 @@ class MLflowStack(Stack):
             vpc_subnets=ec2.SubnetSelection(
                 subnet_type=ec2.SubnetType.PRIVATE_ISOLATED
             ),
-            # multi_az=True,
-            removal_policy=RemovalPolicy.DESTROY,
-            deletion_protection=False,
+            removal_policy=RemovalPolicy.SNAPSHOT,
+            deletion_protection=True,
         )
-        # ==================================================
-        # =============== FARGATE SERVICE ==================
-        # ==================================================
-        cluster = ecs.Cluster(
-            scope=self, id="CLUSTER", cluster_name=cluster_name, vpc=vpc
-        )
-
-        task_definition = ecs.FargateTaskDefinition(
-            scope=self,
-            id="MLflow",
-            task_role=role,
-            cpu=4 * 1024,
-            memory_limit_mib=8 * 1024,
-        )
-
-        container = task_definition.add_container(
-            id="Container",
-            image=ecs.ContainerImage.from_asset(directory="../container/mlflow"),
-            environment={
-                "BUCKET": f"s3://{artifact_bucket.bucket_name}",
-                "HOST": database.db_instance_endpoint_address,
-                "PORT": str(port),
-                "DATABASE": db_name,
-                "USERNAME": username,
-            },
-            secrets={"PASSWORD": ecs.Secret.from_secrets_manager(db_password_secret)},
-            logging=ecs.LogDriver.aws_logs(stream_prefix="mlflow"),
-        )
-        port_mapping = ecs.PortMapping(
-            container_port=5000, host_port=5000, protocol=ecs.Protocol.TCP
-        )
-        container.add_port_mappings(port_mapping)
-
-        fargate_service = ecs_patterns.NetworkLoadBalancedFargateService(
-            scope=self,
-            id="MLFLOW",
-            service_name=service_name,
-            cluster=cluster,
-            task_definition=task_definition,
-        )
-
-        # Setup security group
-        fargate_service.service.connections.security_groups[0].add_ingress_rule(
-            peer=ec2.Peer.ipv4(vpc.vpc_cidr_block),
-            connection=ec2.Port.tcp(5000),
-            description="Allow inbound from VPC for mlflow",
-        )
-
-        # Setup autoscaling policy
-        scaling = fargate_service.service.auto_scale_task_count(max_capacity=2)
-        scaling.scale_on_cpu_utilization(
-            id="AUTOSCALING",
-            target_utilization_percent=70,
-            scale_in_cooldown=Duration.seconds(60),
-            scale_out_cooldown=Duration.seconds(60),
-        )
-        # ==================================================
-        # =================== OUTPUTS ======================
-        # ==================================================
-        CfnOutput(
-            scope=self,
-            id="LoadBalancerDNS",
-            value=fargate_service.load_balancer.load_balancer_dns_name,
-        )
-
 
 
